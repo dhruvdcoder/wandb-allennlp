434caf5781d9ad20c66da7f635adfabbf329d0bb
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-05-11 00:36:11 -0400

    Added entity name in sweep test

--- wandb_allennlp/tests/test_sweep.py
+++ wandb_allennlp/tests/test_sweep.py
@@ -1,5 +1,6 @@
 def test_sweep(script_runner):
-    ret = script_runner.run('wandb', 'agent', '--count=1', 'p285xdjw')
+    ret = script_runner.run('wandb', 'agent', '--count=1',
+                            'dhruveshpate/wandb_allennlp_test/p285xdjw')
     assert ret.success
     assert 'wandb: Program ended successfully.' in ret.stderr
     assert 'wandb: Program failed with code 1' not in ret.stderr
