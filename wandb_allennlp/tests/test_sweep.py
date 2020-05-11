4910730a3eed8e6bb0fad2b9e2bcd6dfa7401e55
Dhruvesh Patel <dhruvdplayer@gmail.com>
2020-05-10 23:55:09 -0400

    Added test setup for PR

---
+++ wandb_allennlp/tests/test_sweep.py
@@ -0,0 +1,5 @@
+def test_foo_bar(script_runner):
+    ret = script_runner.run('wandb', 'agent', '--count=1', 'p285xdjw')
+    assert ret.success
+    assert 'wandb: Program ended successfully.' in ret.stderr
+    assert 'wandb: Program failed with code 1' not in ret.stderr
