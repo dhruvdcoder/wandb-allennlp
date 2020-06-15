5fe55e527417a0d874c67728018a6e897d4103ca
Dhruvesh Patel <dhruvesh@abridge.com>
2020-06-15 11:31:46 -0400

    (WIP) Upgrade to support AllenNLP v1.0

--- wandb_allennlp/allennlp_translator.py
+++ wandb_allennlp/allennlp_translator.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import Tuple, List, Optional, Any
+from typing import Tuple, List, Optional, Any, Callable
 import logging
 import sys
 import os
@@ -9,10 +9,12 @@ from pathlib import Path
 from wandb_allennlp.commandline import Translator
 from allennlp.run import run as allennlp_run
 import json
-logger = logging.getLogger('allennlp_translator')
+
+logger = logging.getLogger("allennlp_translator")
 handler = logging.StreamHandler(sys.stdout)
 handler.setFormatter(
-    logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s"))
+    logging.Formatter("%(asctime)s - %(levelname)s - %(name)s - %(message)s")
+)
 
 logger.addHandler(handler)
 
@@ -23,12 +25,27 @@ else:
 logger.setLevel(LEVEL)
 
 
-class AllenNLPCommandLauncherMixin:
-    def wandb_init(self, program: str, program_args: List[str],
-                   translator_args: argparse.Namespace) -> Any:
+class WandbAllenNLPTranslator(Translator):
+    """Currently specialized only for allennlp train command"""
+
+    def _launch_process(
+        self,
+        program: str,
+        program_args: List[str],
+        translator_args: argparse.Namespace,
+        wandb_init: Callable,
+    ) -> None:
+        run = wandb_init(program, program_args, translator_args)
+        sys.argv = program_args
+        logger.info(f"sys.argv before running allennlp command: \n{sys.argv}")
+        allennlp_run()
+
+    def wandb_init(
+        self, program: str, program_args: List[str], translator_args: argparse.Namespace
+    ) -> Any:
 
         if translator_args.wandb_tags:
-            tags = self.args.wandb_tags.split(',')
+            tags = self.args.wandb_tags.split(",")
         else:
             tags = None
 
@@ -39,66 +56,75 @@ class AllenNLPCommandLauncherMixin:
             tags=tags,
         )
         program_args.append(
-            f'--serialization-dir={Path(wandb_run.dir)/"training_dumps"}')
+            f'--serialization-dir={Path(wandb_run.dir)/"training_dumps"}'
+        )
 
         return wandb_run
 
-    def _launch_process(self, program: str, program_args: List[str],
-                        translator_args: argparse.Namespace) -> None:
-        run = self.wandb_init(program, program_args, translator_args)
-        sys.argv = program_args
-        logger.info(f"sys.argv before running allennlp command: \n{sys.argv}")
-        allennlp_run()
-
-
-class AllenNLPSweepTranslator(AllenNLPCommandLauncherMixin, Translator):
     @classmethod
-    def get_parser(cls, parser: argparse.ArgumentParser = None):
+    def get_parser(
+        cls, parser: argparse.ArgumentParser = None
+    ) -> argparse.ArgumentParser:
         if parser is None:
             parser = argparse.ArgumentParser(description="Run wandb_allennlp")
-        parser.add_argument('--subcommand', default='train')
-        parser.add_argument('--config_file', required=True, type=str)
-        parser.add_argument(
-            '--include-package',
+        allennlp_group = parser.add_argument_group(
+            "allennlp", "Arguments specific to allennlp train command"
+        )
+        allennlp_group.add_argument("--subcommand", default="train")
+        allennlp_group.add_argument("--config_file", required=True, type=str)
+        allennlp_group.add_argument(
+            "--include-package",
             default=[],
-            action='append',
-            help='Same as AllenNLP\'s corresponding argument')
-        parser.add_argument(
-            '--wandb_run_name', help='Name for the run used by wandb')
-        parser.add_argument('--wandb_entity')
-        parser.add_argument('--wandb_project')
-        parser.add_argument('--wandb_sync_tensorboard')
-        parser.add_argument(
-            '--wandb_tags', help='comma seperated list without spaces')
+            action="append",
+            help="Same as AllenNLP's corresponding argument",
+        )
+        wandb_group = parser.add_argument_group(
+            "wandb", "Arguments specific to wandb init"
+        )
+
+        wandb_group.add_argument(
+            "--wandb_run_name",
+            help="Name for the run used by wandb. Will be ignored in sweeps.",
+        )
+        wandb_group.add_argument("--wandb_entity")
+        wandb_group.add_argument("--wandb_project")
+        wandb_group.add_argument(
+            "--wandb_do_not_sync_tensorboard", action="store_true")
+        wandb_group.add_argument(
+            "--wandb_tags", help="comma seperated list without spaces"
+        )
 
         return parser
 
-    def translate(self, translator_args: argparse.Namespace,
-                  program_args: List[str]) -> Tuple[str, List[str]]:
+    def translate(
+        self, translator_args: argparse.Namespace, program_args: List[str]
+    ) -> Tuple[str, List[str]]:
         """At this point only hyperparameter overrides remain
         """
         hparams = {}
-        pattern = re.compile(
-            r'-{1,2}')  # patter for starting -- or - in --key=value
+        # patter for starting -- or - in --key=value
+        pattern = re.compile(r"-{1,2}")
 
         for possible_kwarg in program_args:
-            kw_val = possible_kwarg.split('=')
+            kw_val = possible_kwarg.split("=")
 
             if len(kw_val) == 2:
                 k, v = kw_val
 
-                hparams[pattern.sub('', k)] = v
+                hparams[pattern.sub("", k)] = v
             else:
                 logger.warning(
                     f"{kw_val} not in --key=value form. Will be ignored.")
-        program = 'allennlp'
+        program = "allennlp"
         args = [
-            program, translator_args.subcommand, translator_args.config_file,
+            program,
+            translator_args.subcommand,
+            translator_args.config_file,
             f"--overrides={json.dumps(hparams)}",
-            "--include-package=wandb_allennlp"
+            "--include-package=wandb_allennlp",
         ]
 
         for package in translator_args.include_package:
-            args.append(f'--include-package={package}')
+            args.append(f"--include-package={package}")
 
         return program, args
--- wandb_allennlp/training/callbacks/log_to_wandb.py
+++ wandb_allennlp/training/callbacks/log_to_wandb.py
@@ -1,33 +1,40 @@
-from typing import TYPE_CHECKING, Optional, Callable
-from allennlp.training.callbacks.callback import Callback, handle_event
-from allennlp.training.callbacks.events import Events
+from typing import List, Tuple, Union, Dict, Any, Optional
+from .log import Logger
 import logging
+from allennlp.training.trainer import (
+    GradientDescentTrainer,
+    BatchCallback,
+    EpochCallback,
+)
+
 logger = logging.getLogger(__name__)
 
-if TYPE_CHECKING:
-    from allennlp.training.callback_trainer import CallbackTrainer
-
-
-@Callback.register("log_metrics_to_wandb")
-class LogMetricsToWandb(Callback):
-    def __init__(self, debug=False, debug_log_freq: int = 100) -> None:
+@EpochCallback.register('wandb')
+class LogMetricsToWandb(EpochCallback):
+    def __init__(
+        self, epoch_end_log_freq: int = 1, batch_end_log_freq: int = 100
+    ) -> None:
         # import wandb here to be sure that it was initialized
         # before this line was executed
         super().__init__()
         import wandb  # type: ignore
+
         self.wandb = wandb
-        self.debug = debug
-        self.debug_log_freq = debug_log_freq
-        self._get_batch_num_total: Optional[Callable[[], int]] = None
+        self.epoch_end_log_freq = epoch_end_log_freq
+        self.batch_end_log_freq = batch_end_log_freq
+        self.current_batch_num = -1
+        self.current_epoch_num = -1
+        self.global_step = -1
+        self.previous_logged_global_step = -1
+        self.previous_logged_epoch = -1
 
-    @handle_event(Events.TRAINING_START)
-    def training_start(self, trainer: "CallbackTrainer") -> None:
-        # This is an ugly hack to get the tensorboard instance to know about the trainer, because
-        # the callbacks are defined before the trainer.
-        self._get_batch_num_total = lambda: trainer.batch_num_total
-
-    @handle_event(Events.EPOCH_END, priority=150)  # type: ignore
-    def end_of_epoch_log(self, trainer: "CallbackTrainer") -> None:
+    def __call__(
+        self,
+        trainer: GradientDescentTrainer,
+        metrics: Dict[str, Any],
+        epoch: int,
+        is_master: bool,
+    ) -> None:
         """ This should run after all the epoch end metrics have
         been computed by the metric_tracker callback.
 
@@ -35,29 +42,63 @@ class LogMetricsToWandb(Callback):
         has priority 100. So this callback should have priority > 100
         to be executed after
         """
-        logger.info("Writing metrics to wandb")
-        self.wandb.log(trainer.metrics)
 
-    @handle_event(Events.BATCH_END, priority=1000)  # type: ignore
-    def end_of_batch_log(self, trainer: "CallbackTrainer") -> None:
-        """Log metrics after each mini batch if in debug mode
+        self.current_epoch_num += 1
 
-        The final callback in metric_tracker for EPOCH_END event
-        has priority 100. So this callback should have priority > 100
-        to be executed after
-        """
+        if (
+            is_master
+            and (self.current_epoch_num - self.previous_logged_epoch)
+            >= self.epoch_end_log_freq
+        ):
+            logger.info("Writing metrics for the epoch to wandb")
+            self.wandb.log(
+                {
+                    **metrics,
+                    "epoch_num": self.current_epoch_num,
+                    "batch_num": self.current_batch_num,
+                },
+                step=self.global_step,
+            )
+            self.previous_logged_epoch = self.current_epoch_num
 
-        if self.debug:
-            if self._get_batch_num_total() % self.debug_log_freq == 0:
-                logger.info("Writing metrics to wandb")
-                self.wandb.log(trainer.metrics)
 
-    @handle_event(Events.ERROR, priority=150)
-    def log_after_crash(self, trainer: "CallbackTrainer") -> None:
-        """ We would have tried validation after nan. Log current metrics anyway """
-
-        if isinstance(trainer.exception, ValueError):
-            if str(trainer.exception).strip() == "nan loss encountered":
-                logger.info(
-                    "Nan loss encountered. Logging current metrics to wandb")
-                self.wandb.log(trainer.metrics)
+#    def end_of_batch_log(
+#        self,
+#        trainer: GradientDescentTrainer,
+#        batch_inputs: List[List[TensorDict]],
+#        batch_outputs: List[Dict[str, Any]],
+#        epoch: int,
+#        batch_number: int,
+#        is_training: bool,
+#        is_master: bool,
+#    ) -> None:
+#        """Log metrics after each mini batch if in debug mode
+#
+#        The final callback in metric_tracker for EPOCH_END event
+#        has priority 100. So this callback should have priority > 100
+#        to be executed after
+#        """
+#        pass
+#        # self.current_batch_num += 1
+#        # self.global_step += 1
+#
+#        # if (
+#        #    is_master
+#        #    and (self.global_step - self.previous_logged_global_step)
+#        #    >= self.batch_end_log_freq
+#        # ):
+#        #    logger.info("Writing metrics for the batch to wandb")
+#        #    self.wandb.log(
+#        #        {
+#        #            **metrics,
+#        #            "epoch_num": self.current_epoch_num,
+#        #            "batch_num": self.current_batch_num,
+#        #        },
+#        #        step=self.global_step,
+#        #    )
+#        #    self.previous_logged_epoch = self.current_epoch_num
+#
+#    def log_after_crash(self, trainer: GradientDescentTrainer) -> None:
+#        """ We would have tried validation after nan. Log current metrics anyway """
+#        pass
+#
