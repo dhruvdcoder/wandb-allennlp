7d9e5c8712898bfae2c2661e5bb3337e222bf7d4
Dhruvesh Patel <dhruvesh@abridge.com>
2020-06-15 12:47:50 -0400

    (WIP) Can log config to wandb using the config saved in the config.json

--- wandb_allennlp/training/callbacks/log_to_wandb.py
+++ wandb_allennlp/training/callbacks/log_to_wandb.py
@@ -1,5 +1,9 @@
 from typing import List, Tuple, Union, Dict, Any, Optional
+import json
 import logging
+from copy import deepcopy
+from pathlib import Path
+from allennlp.models.archival import CONFIG_NAME
 from allennlp.training.trainer import (
     GradientDescentTrainer,
     BatchCallback,
@@ -7,6 +11,49 @@ from allennlp.training.trainer import (
 )
 
 logger = logging.getLogger(__name__)
+Number = Union[int, float]
+Value = Union[int, float, bool, str]
+
+
+def _flatten_dict(params: Dict[str, Any], delimiter: str = ".") -> Dict[str, Value]:
+    """
+    Flatten hierarchical dict, e.g. ``{'a': {'b': 'c'}} -> {'a.b': 'c'}``.
+    Args:
+        params: Dictionary containing the hyperparameters
+        delimiter: Delimiter to express the hierarchy. Defaults to ``'/'``.
+    Returns:
+        Flattened dict.
+    """
+    output: Dict[str, Union[str, Number]] = {}
+
+    def populate(
+        inp: Union[Dict[str, Any], List, str, Number, bool], prefix: List[str]
+    ) -> None:
+        if isinstance(inp, dict):
+            for k, v in inp.items():
+                populate(v, deepcopy(prefix) + [k])
+
+        elif isinstance(inp, list):
+            for i, val in enumerate(inp):
+                populate(val, deepcopy(prefix) + [str(i)])
+        elif isinstance(inp, (str, float, int, bool)):
+            output[delimiter.join(prefix)] = inp
+        else:  # unsupported type
+            raise ValueError(
+                f"Unsuported type {type(inp)} at {delimiter.join(prefix)} for flattening."
+            )
+
+    populate(params, [])
+
+    return output
+
+
+def get_config_from_serialization_dir(dir_: str,) -> Dict[str, Value]:
+    with open(Path(dir_) / CONFIG_NAME) as f:
+        config_dict = json.load(f)
+    config_dict = _flatten_dict(config_dict)
+
+    return config_dict
 
 
 @EpochCallback.register("wandb")
@@ -19,12 +66,23 @@ class LogMetricsToWandb(EpochCallback):
         super().__init__()
         import wandb  # type: ignore
 
+        self.config: Optional[Dict[str, Value]] = None
+
         self.wandb = wandb
         self.epoch_end_log_freq = epoch_end_log_freq
         self.batch_end_log_freq = batch_end_log_freq
         self.current_batch_num = -1
         self.current_epoch_num = -1
         self.previous_logged_epoch = -1
+
+    def update_config(self, trainer: GradientDescentTrainer) -> None:
+        if self.config is None:
+            # we assume that allennlp train pipeline would have written
+            # the entire config to the file by this time
+            logger.info(f"Sending config to wandb...")
+            self.config = get_config_from_serialization_dir(
+                trainer._serizalization_dir)
+            self.wandb.config.update(self.config)
 
     def __call__(
         self,
@@ -40,6 +98,9 @@ class LogMetricsToWandb(EpochCallback):
         has priority 100. So this callback should have priority > 100
         to be executed after
         """
+
+        if self.config is None:
+            self.update_config(trainer)
 
         self.current_epoch_num += 1
 
