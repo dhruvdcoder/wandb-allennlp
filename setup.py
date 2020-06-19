b6a33f643d095399075ca5abe270ea5a17206024
Dhruvesh <dhruvdplayer@gmail.com>
2020-06-19 10:26:16 -0400

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ install_requires = [
 
 setup(
     name='wandb_allennlp',
-    version='0.1.0',
+    version='0.1.2',
     author="Dhruvesh Patel",
     author_email="1793dnp@gmail.com",
     description='Utilities to use allennlp with wandb',
